a01a0408ea338c4f73e6d578702942c4b98dc3f2
R. Nicolás Fernandez C <rnfernandezc@hotmail.com>
2019-03-27 06:12:26 -0300

    metodo index

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('dashboard');
 });
 
-Route::resource('task', 'TaskController', ['except' => 'show']);
+Route::resource('tasks', 'TaskController', ['except' => 'show']);
